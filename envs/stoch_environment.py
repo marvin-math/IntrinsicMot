40f72b12da46614cbded367100217ef84ecba6af
marvin-math <marvin@mathony.ms>
2024-09-26 09:15:23 +0200

    started working on stochastic environment. Plot now shows several individual plots per segment

---
+++ envs/stoch_environment.py
@@ -0,0 +1,222 @@
+import numpy as np
+import random
+import gym
+from gym import spaces
+
+class MDPAlirezaStoch(gym.Env):
+    # To Do: figure out how to implement episode logic (has to do with reset)
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+
+    def __init__(self, render_mode=None, size=11):
+        self.size = size  # The size of the square grid
+        self.window_size = 512  # The size of the PyGame window
+
+        # Observations are dictionaries with the envs's and the target's location.
+        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
+        self.observation_space = spaces.Dict(
+            {
+                "envs": spaces.Box(0, size - 1, shape=(1,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(1,), dtype=int),
+            }
+        )
+
+        # We have 4 actions, corresponding to "right", "up", "left", "down"
+        self.action_space = spaces.Discrete(3)
+        self.progress_states = [0, 1, 2, 3, 4, 5]
+        self.trap_states = [6, 7]
+        self.goal_states = [8, 9, 10]
+        self.stochastic_states = list(range(20, 70))
+        self.inv_goal_prob = 0.02
+        self.probabilities = [1 - self.inv_goal_prob, self.inv_goal_prob / 2, self.inv_goal_prob / 2]
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        # create dictionary of action spaces. One for each state.
+        # potential problem: two actions here have the same move. This is different in trap states, where each
+        # action corresponds to unique move
+        available_actions_progress = [np.array([0, 0]), np.array([0, 1]), np.array([1, 0])]
+
+
+        self._action_to_direction = {
+            'state_0': {0: None,
+                        1: None,
+                        2: None},
+            'state_1': {0: None,
+                        1: None,
+                        2: None},
+            'state_2': {0: None,
+                        1: None,
+                        2: None},
+            'state_3': {0: None,
+                        1: None,
+                        2: None},
+            'state_4': {0: None,
+                        1: None,
+                        2: None},
+            'state_5': {0: None,
+                        1: None,
+                        2: None}
+        }
+
+        # have to implement some shuffle logic - not every state has same action transition mapping
+        # one action makes agent stay, one makes agent progress and one makes agent go to trap state
+        # To Do: ensure that this happens each episode
+        for state_key in self._action_to_direction:
+            np.random.shuffle(available_actions_progress)
+            counter = 0
+            state_dict = self._action_to_direction[state_key]
+            for d in state_dict:
+                if state_dict[d] is None:
+                    state_dict[d] = available_actions_progress[counter]
+                    counter += 1
+
+
+        # also have to implement shuffle logic
+        # two actions make agent stay in trap states (either same or different trap state), one action makes agent go to state 1
+        available_actions_trap = [np.array([0, 0]), np.array([1, 0]), np.array([0, 1])]
+
+        self._action_to_direction_trap = {
+            'state_6': {0: None,
+                        1: None,
+                        2: None},
+            'state_7': {0: None,
+                        1: None,
+                        2: None}
+        }
+
+        for state_key in self._action_to_direction_trap:
+            np.random.shuffle(available_actions_trap)
+            counter = 0
+            state_dict = self._action_to_direction_trap[state_key]
+            for d in state_dict:
+                if state_dict[d] is None:
+                    state_dict[d] = available_actions_trap[counter]
+                    counter += 1
+
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+
+        self.render_mode = render_mode
+
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.
+        """
+        self.window = None
+        self.clock = None
+
+    def _get_obs(self):
+        return {"envs": self._agent_location, "target": self._target_location}
+
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(
+                self._agent_location - self._target_location, ord=1
+            )
+        }
+
+    def reset(self, seed=None, options=None):
+
+        # randomly choose whether start location is state 1 or 2
+        self._agent_location = random.choice(np.array([1, 0], np.array([1,1])))
+
+        # target location
+        self._target_locations = [np.array([1, 8]), np.array([1, 9]), np.array([1, 10])]
+        np.random.shuffle(self.progress_states)
+        np.random.shuffle(self.trap_states)
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, info
+
+
+    def step(self, action):
+        # the following first checks whether the envs is currently in a progressing state or trap state
+        # it then checks the action and depending on which action is taken from where it determines the next state
+        # IMPORTANT: is self._agent_location up to date? Is it the state that the envs is in before taking this action?
+        agent_absolute = False
+        state_key = f"state_{self._agent_location[1]}"
+        progress = True
+        if self._agent_location[1] in self.progress_states:
+
+            if np.array_equal(self._action_to_direction[state_key][action], np.array([1, 0])):
+                # bad action - transported to trap state
+                # To Do: use direction variable; check boundaries
+                agent_absolute = True
+                # randomize which trap state an envs is transported to from which progressing state upon taking bad action
+                if self._agent_location[1] in (self.progress_states[0], self.progress_states[1], self.progress_states[2]):
+                    self._agent_location = np.array([1, self.trap_states[0]])
+                elif self._agent_location[1] in (self.progress_states[2], self.progress_states[3], self.progress_states[4]):
+                    self._agent_location = np.array([1, self.trap_states[1]])
+            elif np.array_equal(self._action_to_direction[state_key][action], np.array([0, 0])):
+                agent_absolute = True
+                if self._agent_location[1] == 4:
+                    self._agent_location = random.choice(list_stochastic_states) # TODO - create list of stochastic states
+                else:
+                    self._agent_location = self._agent_location
+            elif np.array_equal(self._action_to_direction[state_key][action], np.array([0, 1])):
+                if self._agent_location[1] == 5:
+                    agent_absolute = True
+                    self._agent_location[1] = np.random.choice(self.goal_states, p = self.probabilities)
+
+
+        elif self._agent_location[1] in self.trap_states:
+            progress = False
+            # envs is in a trap state
+            if np.array_equal(self._action_to_direction_trap[state_key][action], np.array([-1, 0])):
+                #stay at position
+                agent_absolute = True
+                self._agent_location = self._agent_location
+            elif np.array_equal(self._action_to_direction_trap[state_key][action], np.array([0, 0])):
+                # good action - transported to state 1
+                agent_absolute = True
+                self._agent_location = np.array([1, 0])
+            elif np.array_equal(self._action_to_direction_trap[state_key][action], np.array([0, -1])):
+                # one to the left
+                if self._agent_location[1] not in (8, 9):
+                    agent_absolute = True
+                    self._agent_location[1] = 9
+            elif np.array_equal(self._action_to_direction_trap[state_key][action], np.array([0, 1])):
+                # one to the right
+                if self._agent_location[1] not in (7, 8):
+                    agent_absolute = True
+                    self._agent_location[1] = 7
+
+            # Map the action (element of {0,1,2,3}) to the direction we walk in
+
+        if not agent_absolute:
+            # In theory, I do not have to make sure that envs doesn't leave environment, because this is taken
+            # care of in the steps. It never has an action available with which it can leave environment
+
+            direction = self._action_to_direction[state_key][action] if progress else self._action_to_direction_trap[state_key][action]
+            self._agent_location = self._agent_location + direction
+
+
+
+        # An episode is done iff the envs has reached the target
+        terminated = np.array_equal(self._agent_location, self._target_location)
+        # think about the implementation of reward here - same name as reward in the agent, but different concept
+        reward = 1 if terminated else 0  # Binary sparse rewards
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, reward, terminated, False, info
+
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+
+
--- run.py
+++ run.py
@@ -5,6 +5,11 @@ import q_learning_agent
 import envs.environment
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+
+# Directory to save the plots
+output_dir = '/Users/marvinmathony/Documents/RLandNNs/plots'
+os.makedirs(output_dir, exist_ok=True)  # Create directory if it doesn't exist
 
 # parameters needed by our policy and learning rule
 params_surprise = {
@@ -68,105 +73,181 @@ for segment in segments:
     average_novelty_counts[segment] /= n_runs
 
 # List of trial segments
-segments_first = ['50', '100', '200', '500']  # First set of segments
-segments_second = ['1000', '2000', '5000', '10000']  # Second set of segments
+segments = ['50', '100', '200', '500', '1000', '2000', '5000', '10000']  # Trial segments
+states = np.arange(11)  # Assuming there are 11 states
 
-states = np.arange(11)  # Assuming there are 11 states, adjust this as needed
+# Colors for each group
+color_groups = {
+    'group_1_3': sns.color_palette("muted", 3)[0],  # Color for states 1-3
+    'group_4_6': sns.color_palette("muted", 3)[1],  # Color for states 4-6
+    'group_7_9': sns.color_palette("muted", 3)[2],  # Color for states 7-9
+}
 
-# Colors for states (same color for the states across both agents)
-state_colors = sns.color_palette("muted", len(states))
+# Loop through each segment and plot separately
+for segment in segments:
+    fig, ax = plt.subplots(figsize=(8, 6))
 
-# --- First Plot: Segments 50 to 500 ---
-
-fig, ax1 = plt.subplots(figsize=(10, 6))
-
-# Positions for the trial segments on the x-axis
-bar_width = 0.4
-r1 = np.arange(len(segments_first))
-r2 = [x + bar_width for x in r1]
-
-# Loop through each trial segment and create stacked bars
-for i, segment in enumerate(segments_first):
     surprise_data = average_surprise_counts[segment]
     novelty_data = average_novelty_counts[segment]
 
-    # Stacked bar for surprise agent
-    bottom = np.zeros(len(segments_first))
-    for state_idx, color in enumerate(state_colors):
-        ax1.bar(r1[i], surprise_data[state_idx], color=color, width=bar_width, edgecolor='grey',
-                label=f'State {state_idx}' if i == 0 else "", bottom=bottom[i])
-        bottom[i] += surprise_data[state_idx]
+    # Bar width
+    bar_width = 0.0005
+    r1 = np.arange(1)  # Single bar for surprise agent
+    r2 = [x + bar_width for x in r1]  # Single bar for novelty agent
 
-    # Stacked bar for novelty agent
-    bottom = np.zeros(len(segments_first))
-    for state_idx, color in enumerate(state_colors):
-        ax1.bar(r2[i], novelty_data[state_idx], color=color, width=bar_width, edgecolor='grey',
-                bottom=bottom[i])
-        bottom[i] += novelty_data[state_idx]
+    # Group states together
+    surprise_group_1_3 = np.sum(surprise_data[0:4])  # Sum states 1, 2, 3
+    surprise_group_4_6 = np.sum(surprise_data[4:7])  # Sum states 4, 5, 6
+    surprise_group_7_9 = np.sum(surprise_data[7:10])  # Sum states 7, 8, 9
 
-# Add labels for agents
-for i in range(len(segments_first)):
-    ax1.text(r1[i], -700, 'Surprise', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
-    ax1.text(r2[i], -700, 'Novelty', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
-
-# Add labels and title for the first plot
-ax1.set_xlabel('Trial Segments (50 to 500)', fontweight='bold')
-ax1.set_ylabel('State Visits', fontweight='bold')
-ax1.set_title(f'State Visits per Trial Segment (50 to 500), averaged over {n_runs} agents', fontweight='bold')
-
-# Custom x-axis labels
-ax1.set_xticks([r + bar_width / 2 for r in range(len(segments_first))])
-ax1.set_xticklabels(segments_first)
-
-# Show legend for the states (only display once)
-handles, labels = ax1.get_legend_handles_labels()
-ax1.legend(handles[:len(states)], labels[:len(states)], title="States")
-
-# --- Second Plot: Segments 1000 to 10000 ---
-
-fig, ax2 = plt.subplots(figsize=(10, 6))
-
-# Positions for the trial segments on the x-axis
-r1 = np.arange(len(segments_second))
-r2 = [x + bar_width for x in r1]
-
-# Loop through each trial segment and create stacked bars
-for i, segment in enumerate(segments_second):
-    surprise_data = average_surprise_counts[segment]
-    novelty_data = average_novelty_counts[segment]
+    novelty_group_1_3 = np.sum(novelty_data[0:4])
+    novelty_group_4_6 = np.sum(novelty_data[4:7])
+    novelty_group_7_9 = np.sum(novelty_data[7:10])
 
     # Stacked bar for surprise agent
-    bottom = np.zeros(len(segments_second))
-    for state_idx, color in enumerate(state_colors):
-        ax2.bar(r1[i], surprise_data[state_idx], color=color, width=bar_width, edgecolor='grey',
-                label=f'State {state_idx}' if i == 0 else "", bottom=bottom[i])
-        bottom[i] += surprise_data[state_idx]
+    ax.bar(r1, surprise_group_1_3, color=color_groups['group_1_3'], width=bar_width, edgecolor='grey',
+           label='States 1-3')
+    ax.bar(r1, surprise_group_4_6, bottom=surprise_group_1_3, color=color_groups['group_4_6'], width=bar_width,
+           edgecolor='grey', label='States 4-7')
+    ax.bar(r1, surprise_group_7_9, bottom=surprise_group_1_3 + surprise_group_4_6, color=color_groups['group_7_9'],
+           width=bar_width, edgecolor='grey', label='Trap States')
 
     # Stacked bar for novelty agent
-    bottom = np.zeros(len(segments_second))
-    for state_idx, color in enumerate(state_colors):
-        ax2.bar(r2[i], novelty_data[state_idx], color=color, width=bar_width, edgecolor='grey',
-                bottom=bottom[i])
-        bottom[i] += novelty_data[state_idx]
+    ax.bar(r2, novelty_group_1_3, color=color_groups['group_1_3'], width=bar_width, edgecolor='grey')
+    ax.bar(r2, novelty_group_4_6, bottom=novelty_group_1_3, color=color_groups['group_4_6'], width=bar_width,
+           edgecolor='grey')
+    ax.bar(r2, novelty_group_7_9, bottom=novelty_group_1_3 + novelty_group_4_6, color=color_groups['group_7_9'],
+           width=bar_width, edgecolor='grey')
 
-# Add labels for agents
-for i in range(len(segments_second)):
-    ax2.text(r1[i], -700, 'Surprise', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
-    ax2.text(r2[i], -700, 'Novelty', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
+    # Add labels for agents
+    ax.text(r1[0], -(0.03*int(segment)), 'Surprise', ha='center', va='center', color='black')
+    ax.text(r2[0], -(0.03*int(segment)), 'Novelty', ha='center', va='center', color='black')
 
-# Add labels and title for the second plot
-ax2.set_xlabel('Trial Segments (1000 to 10000)', fontweight='bold')
-ax2.set_ylabel('State Visits', fontweight='bold')
-ax2.set_title(f'State Visits per Trial Segment (1000 to 10000), averaged over {n_runs} agents', fontweight='bold')
+    # Set labels and title
+    ax.set_ylabel('State Visits', fontweight='bold')
+    ax.set_title(f'State Visits in Segment {segment}', fontweight='bold')
 
-# Custom x-axis labels
-ax2.set_xticks([r + bar_width / 2 for r in range(len(segments_second))])
-ax2.set_xticklabels(segments_second)
+    # Custom x-axis labels
+    ax.set_xticks([r + bar_width / 2 for r in range(1)])
+    ax.set_xticklabels(['Agents'])
 
-# Show legend for the states (only display once)
-handles, labels = ax2.get_legend_handles_labels()
-ax2.legend(handles[:len(states)], labels[:len(states)], title="States")
+    # Show legend for the state groups
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[:3], labels[:3], title="State Groups")
 
-# Show the plots
-plt.tight_layout()
-plt.show()
+    filename = os.path.join(output_dir, f'plot_segment_{segment}.png')
+    plt.savefig(filename, format='png')
+    print(f'Saved plot for segment {segment} as {filename}')
+    plt.clf()
+
+
+
+
+
+
+
+
+def plot_stacked_individual_bars_two_images():
+
+    # List of trial segments
+    segments_first = ['50', '100', '200', '500']  # First set of segments
+    segments_second = ['1000', '2000', '5000', '10000']  # Second set of segments
+
+    states = np.arange(11)  # Assuming there are 11 states, adjust this as needed
+
+    # Colors for states (same color for the states across both agents)
+    state_colors = sns.color_palette("muted", len(states))
+
+    # --- First Plot: Segments 50 to 500 ---
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    # Positions for the trial segments on the x-axis
+    bar_width = 0.4
+    r1 = np.arange(len(segments_first))
+    r2 = [x + bar_width for x in r1]
+
+    # Loop through each trial segment and create stacked bars
+    for i, segment in enumerate(segments_first):
+        surprise_data = average_surprise_counts[segment]
+        novelty_data = average_novelty_counts[segment]
+
+        # Stacked bar for surprise agent
+        bottom = np.zeros(len(segments_first))
+        for state_idx, color in enumerate(state_colors):
+            ax1.bar(r1[i], surprise_data[state_idx], color=color, width=bar_width, edgecolor='grey',
+                    label=f'State {state_idx}' if i == 0 else "", bottom=bottom[i])
+            bottom[i] += surprise_data[state_idx]
+
+        # Stacked bar for novelty agent
+        bottom = np.zeros(len(segments_first))
+        for state_idx, color in enumerate(state_colors):
+            ax1.bar(r2[i], novelty_data[state_idx], color=color, width=bar_width, edgecolor='grey',
+                    bottom=bottom[i])
+            bottom[i] += novelty_data[state_idx]
+
+    # Add labels for agents
+    for i in range(len(segments_first)):
+        ax1.text(r1[i], -700, 'Surprise', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
+        ax1.text(r2[i], -700, 'Novelty', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
+
+    # Add labels and title for the first plot
+    ax1.set_xlabel('Trial Segments (50 to 500)', fontweight='bold')
+    ax1.set_ylabel('State Visits', fontweight='bold')
+    ax1.set_title(f'State Visits per Trial Segment (50 to 500), averaged over {n_runs} agents', fontweight='bold')
+
+    # Custom x-axis labels
+    ax1.set_xticks([r + bar_width / 2 for r in range(len(segments_first))])
+    ax1.set_xticklabels(segments_first)
+
+    # Show legend for the states (only display once)
+    handles, labels = ax1.get_legend_handles_labels()
+    ax1.legend(handles[:len(states)], labels[:len(states)], title="States")
+
+    # --- Second Plot: Segments 1000 to 10000 ---
+
+    fig, ax2 = plt.subplots(figsize=(10, 6))
+
+    # Positions for the trial segments on the x-axis
+    r1 = np.arange(len(segments_second))
+    r2 = [x + bar_width for x in r1]
+
+    # Loop through each trial segment and create stacked bars
+    for i, segment in enumerate(segments_second):
+        surprise_data = average_surprise_counts[segment]
+        novelty_data = average_novelty_counts[segment]
+
+        # Stacked bar for surprise agent
+        bottom = np.zeros(len(segments_second))
+        for state_idx, color in enumerate(state_colors):
+            ax2.bar(r1[i], surprise_data[state_idx], color=color, width=bar_width, edgecolor='grey',
+                    label=f'State {state_idx}' if i == 0 else "", bottom=bottom[i])
+            bottom[i] += surprise_data[state_idx]
+
+        # Stacked bar for novelty agent
+        bottom = np.zeros(len(segments_second))
+        for state_idx, color in enumerate(state_colors):
+            ax2.bar(r2[i], novelty_data[state_idx], color=color, width=bar_width, edgecolor='grey',
+                    bottom=bottom[i])
+            bottom[i] += novelty_data[state_idx]
+
+    # Add labels for agents
+    for i in range(len(segments_second)):
+        ax2.text(r1[i], -700, 'Surprise', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
+        ax2.text(r2[i], -700, 'Novelty', ha='center', rotation=90, va='center', color='black', rotation_mode="anchor")
+
+    # Add labels and title for the second plot
+    ax2.set_xlabel('Trial Segments (1000 to 10000)', fontweight='bold')
+    ax2.set_ylabel('State Visits', fontweight='bold')
+    ax2.set_title(f'State Visits per Trial Segment (1000 to 10000), averaged over {n_runs} agents', fontweight='bold')
+
+    # Custom x-axis labels
+    ax2.set_xticks([r + bar_width / 2 for r in range(len(segments_second))])
+    ax2.set_xticklabels(segments_second)
+
+    # Show legend for the states (only display once)
+    handles, labels = ax2.get_legend_handles_labels()
+    ax2.legend(handles[:len(states)], labels[:len(states)], title="States")
+
+    # Show the plots
+    return plt.tight_layout(), plt.show()
